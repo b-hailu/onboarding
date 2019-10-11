ba5293a46ee93925a169c38aeaebaaa265921863
Belaynesh <bhailu103@gmail.com>
2019-10-11 15:43:08 -0500

    1_black_jack

---
+++ week_1/Belaynesh/1_black_jack.rb
@@ -0,0 +1,12 @@
+def blackj()
+    int1 = 25 
+    int2 = 20
+
+    if (int1 >= 0 && int2 <= 20 )
+        return 0
+    else
+        puts " The number is working"
+        
+
+end    
+puts blackj
