c3d2d4353124202480e0ac77efb9eb91a06d36cc
Belaynesh Hailu <belaynesh@snapit.solutions>
2019-10-23 12:08:35 -0500

    conditional statement exercise

---
+++ week_1/Belaynesh/1_codecadamyexer.rb
@@ -0,0 +1,7 @@
+def code_my_exe()
+    my_num = 35
+    my_boolean = true
+    my_string = "Ruby"
+    puts "#{my_num} , #{my_boolean}, #{my_string}
+end
+puts  code_my_exe
---
+++ week_1/Belaynesh/1_ifexercise.rb
@@ -0,0 +1,6 @@
+def exerciseif()
+    a = get.chomp.to_i
+    if a >= 18
+        puts " you are allowed to stay home"
+    else
+        pust "you can not stay home by yourself"
